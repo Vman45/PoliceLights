26ee3c66a9430ea36095fbfe5747b3205eda0643
Seth <setsmith@tesla.com>
2020-09-13 17:56:24 -0700

    Flash

--- policelights.py
+++ policelights.py
@@ -1,3 +1,16 @@
+from gpiozero import LED
+from time import sleep
+
+
+def flashlights():
+    led = LED(4)
+    led.on()
+    sleep(0.1)
+    led.off()
+    sleep(0.1)
+
+
+
 if __name__ == "__main__":
-    print("Changes")
-    
+    for x in range(0,50):
+        flashlights()
